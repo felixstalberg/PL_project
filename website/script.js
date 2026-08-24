b21db21c9565e8e77f61cb691b8bd46557c83b25
Felix Stalberg <felix.stalberg@gmail.com>
2026-08-24 14:38:53 +0200

    testa nya filer

---
+++ website/script.js
@@ -0,0 +1,20 @@
+fetch("../data/pl_data.json")
+    .then(response => response.json())
+    .then(data => {
+
+        const topScorers = data.top_scorers;
+
+        const container = document.getElementById("top-scorers");
+
+        topScorers.forEach((player, index) => {
+
+            const row = document.createElement("p");
+
+            row.textContent =
+                `${index + 1}. ${player.first_name} ${player.second_name} - ${player.goals_scored} mål`;
+
+            container.appendChild(row);
+
+        });
+
+    });
