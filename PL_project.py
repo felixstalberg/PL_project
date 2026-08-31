f7cf78ecd1380b92b1131689a42a991bd763412c
Felix Stalberg <felix.stalberg@gmail.com>
2026-08-31 16:10:00 +0200

    match_df+lägg till i json

--- PL_project.py
+++ PL_project.py
@@ -1,13 +1,17 @@
-#PL project
-
 import requests
 import matplotlib as plt
 import pandas as pd
 import json
 
+#API för statistik
 url = "https://fantasy.premierleague.com/api/bootstrap-static/"
 response = requests.get(url)
 data = response.json()
+
+#API för matchstatistik
+fixtures_url = "https://fantasy.premierleague.com/api/fixtures/"
+fixtures_response = requests.get(fixtures_url)
+fixtures = fixtures_response.json()
 
 #print(data.keys()) #--> dict_keys(['chips', 'events', 'game_settings', 'game_config', 'phases', 'teams',
                                    #'total_players', 'element_stats', 'element_types', 'elements'])
@@ -17,6 +21,26 @@ player_features = pd.DataFrame(data["elements"])
 
 #ta ut alla features som finns för lagen
 team_features = pd.DataFrame(data["teams"])
+
+"#Matcher"
+fixtures_df = pd.DataFrame(fixtures)
+
+matches = fixtures_df[["event", "team_h", "team_a", "team_h_score", "team_a_score"]].copy()
+
+matches = matches.rename(columns={"event": "gameweek"})
+
+home_teams = team_features[["id", "name"]].rename(
+    columns={"id": "team_h", "name": "home_team"}
+)
+
+away_teams = team_features[["id", "name"]].rename(
+    columns={"id": "team_a", "name": "away_team"}
+)
+#ersätt team_h/team_a (id) till riktiga namn mha merge
+matches = matches.merge(home_teams, on="team_h")
+matches = matches.merge(away_teams, on="team_a")
+
+matches = matches.drop(columns=["team_h", "team_a"])
 
 "Tabell"
 table = team_features.sort_values("position") #sortera team_features efter position
@@ -53,7 +77,9 @@ pl_data = {
     ].to_dict(orient="records"),
 
     "table": table[["position", "name"]
-    ].to_dict(orient="records")
+    ].to_dict(orient="records"),
+
+    "matches": matches.to_dict(orient="records")
 }
 
 with open("data/pl_data.json", "w", encoding="utf-8") as file: 
